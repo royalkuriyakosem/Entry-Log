b7e0dc0c5e658548458c7cbaa5ad72195650526c
royalkuriyakosem <royalkuriyakosem@gmail.com>
2024-06-28 11:08:19 +0530

    third commit

--- app/src/main/java/com/example/entrylog/MainActivity.java
+++ app/src/main/java/com/example/entrylog/MainActivity.java
@@ -1,19 +1,36 @@
 package com.example.entrylog;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.AppCompatButton;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
 
+    EditText ed1,ed2;
+    AppCompatButton b1;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
+        ed1=(EditText) findViewById(R.id.uname);
+        ed2=(EditText) findViewById(R.id.pass);
+        b1=(AppCompatButton) findViewById(R.id.logbtn);
+        b1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String userName=ed1.getText().toString();
+                String passWord=ed2.getText().toString();
+                Toast.makeText(getApplicationContext(),userName +  " " + passWord, Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 }
